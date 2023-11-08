306383bef6862dcb35dd0bda33c44b978051acfe
Alarlar <52812499+Alarlar@users.noreply.github.com>
2023-11-07 21:01:13 -0800

    sunnySnowDay

---
+++ sunnySnowDay.js
@@ -0,0 +1,15 @@
+//background color
+background(166, 217, 230);
+// The ground
+fill(52, 186, 110);
+rect(0, 300, 400, 100);  
+
+// The sun
+fill(247, 231, 8);
+ellipse(80, 64, 100, 100);  
+
+// The snowman
+fill(242, 242, 242);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
