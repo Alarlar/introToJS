eb270e221bb69763d38bf086e4cfcb1adfa1af67
Alarlar <52812499+Alarlar@users.noreply.github.com>
2023-11-08 08:41:59 -0800

    projectWhatsForDinner

---
+++ projectWhatsForDinner.js
@@ -0,0 +1,18 @@
+background(224, 206, 150); // wooden table
+
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300);
+noStroke();
+//Potatos
+fill(255, 242, 0);
+ellipse(200, 110, 51, 49);
+ellipse(156, 78, 51, 49);
+ellipse(156, 151, 51, 49);
+//Meat
+fill(232, 148, 148);
+rect(111, 200, 179, 71);
+//Green
+strokeWeight(13);
+stroke(46, 194, 56);
+line(90, 183, 110, 46);
+line(117, 188, 116, 46);
