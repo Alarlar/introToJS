124e276b71acde77cfe948ba653dc7ff1e2c45fd
Alarlar <52812499+Alarlar@users.noreply.github.com>
2023-11-19 18:34:46 -0800

    challengeSayYourName

---
+++ challengeSayYourName.js
@@ -0,0 +1,16 @@
+    var showName = function() {
+        
+        var textX = random(0, 300);
+    var textY = random(14, 300);
+    var yourName = "Dinar";
+    
+    fill(255, 0, 0);
+    textSize(30);
+    text("Hiiii, " + yourName, textX, textY);
+        
+    };
+    showName();
+    showName();
+    showName();
+    showName();
+    
