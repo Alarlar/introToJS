735d2aedc846d8f898a904683a8d52061d074295
Alarlar <52812499+Alarlar@users.noreply.github.com>
2023-11-30 18:45:34 -0800

    challengeLoopyLandscape

---
+++ challengeLoopyLandscape.js
@@ -0,0 +1,18 @@
+background(144, 240, 234);
+
+var x = 0;
+
+// Draw the sun
+noStroke();
+fill(255, 140, 0);
+ellipse(335, 66, 70, 70);
+
+// Get images from library and remember in variables
+var grass = getImage("cute/GrassBlock");
+var tree = getImage("cute/TreeUgly");
+
+while(x < 400) {
+    image(grass, x, 270);
+    image(tree, x, 200);
+    x += 100;
+}
