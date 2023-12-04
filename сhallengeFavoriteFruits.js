585dee81a85336418777db33f19faf13c728ceb9
Alarlar <52812499+Alarlar@users.noreply.github.com>
2023-12-04 09:50:40 -0800

    ChallengeFavoriteFruits

---
+++ сhallengeFavoriteFruits.js
@@ -0,0 +1,9 @@
+var myFruit = ["Banana", "Mango", "Watermelon"];
+
+fill(255, 0, 0);
+text(myFruit[0], 10, 20);
+text(myFruit[1], 10, 60);
+text(myFruit[2], 10, 100);
+
+text("My top " +  myFruit.length + " favorite fruits", 10, 140);
+
